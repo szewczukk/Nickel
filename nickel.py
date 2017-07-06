7f58a13ff4490d30796f293be6b23805f8355ee7
bjornus <dmitro.szewczuk@wp.pl>
2017-07-06 23:24:10 +0200

    Init command added

--- nickel.py
+++ nickel.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
 from argparse import *
+from xml.etree import ElementTree
+from os import makedirs, system, path
 
 parser = ArgumentParser(description="Type command")
 parser.add_argument("command", help="\"check\" for print all tasks or \"add\" to add new task")
 
 arguments = parser.parse_args()
 if arguments.command == "check":
-    print ("Check")
+    tree = ElementTree.parse("~/nickel/tasks.xml")
 
+if arguments.command == "init":
+    if not path.exists("/home/.nickel"):
+        makedirs("/home/.nickel")
+
+    with open("/home/.nickel/tasks.xml", "w") as f:
+        f.write("<?xml version=\"1.0\"?>\n<tasks> \n</tasks>")
+
+    print("Path initialized!")
 elif arguments.command == "add":
     print ("Adding")
