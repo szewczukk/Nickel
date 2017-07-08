0931c29acb556d1e685c393a3d3c89cecaca482b
bjornus <dmitro.szewczuk@wp.pl>
2017-07-08 21:14:38 +0200

    Creating .nickel catalog in home user directory

--- nickel.py
+++ nickel.py
@@ -6,11 +6,11 @@ from os import makedirs, path
 
 parser = ArgumentParser(description="Type command")
 parser.add_argument("command")
-parser.add_argument("-project", required=False)
+parser.add_argument("project")
 
 arguments = parser.parse_args()
 if arguments.command == "check":
-    tree = ElementTree.parse("/home/.nickel/tasks.xml")
+    tree = ElementTree.parse(path.expanduser("~") + "/.nickel/tasks.xml")
     root = tree.getroot()
     iterator = 0
 
@@ -19,6 +19,9 @@ if arguments.command == "check":
         iterator += 1
 
         project = str(task.get("project"))
+        given_project = arguments.project
+
+        print(given_project)
 
         if project != "None":
             print(str(iterator) + ": " + task.text + " : " + project)
@@ -26,20 +29,20 @@ if arguments.command == "check":
             print(str(iterator) + ": " + task.text + " : ")
 
 if arguments.command == "init":
-    if not path.exists("/home/.nickel"):
-        makedirs("/home/.nickel")
+    if not path.exists(path.expanduser("~") + "/.nickel"):
+        makedirs(path.expanduser("~") + "/.nickel")
 
     root = ElementTree.Element("root")
 
     tree = ElementTree.ElementTree(root)
-    tree.write("/home/.nickel/tasks.xml")
+    tree.write(path.expanduser("~") + "/.nickel/tasks.xml")
 
     print("Path initialized!")
 elif arguments.command == "add":
-    tree = ElementTree.parse("/home/.nickel/tasks.xml")
+    tree = ElementTree.parse(path.expanduser("~") + "/.nickel/tasks.xml")
     root = tree.getroot()
 
     new_task = ElementTree.Element("task", {"project": arguments.project})
     new_task.text = raw_input("Type new task: ")
     root.append(new_task)
-    tree.write("/home/.nickel/tasks.xml")
+    tree.write("/home/User/.nickel/tasks.xml")
