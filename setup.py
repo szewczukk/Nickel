1ed09913245e21c4f9a9a30fc436fc7497b02b75
bjornus <dmitro.szewczuk@wp.pl>
2017-07-08 21:42:10 +0200

    Changes in setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
 
 setup(
+    name="Nickel",
+    version="1.0",
+    author="Dmitro bjornus Szewczuk",
+    author_email="dmitro.szewczuk@gmail.com",
+    url="https://github.com/bjornus/Nickel",
     scripts=[
         "nickel.py"
-    ]
+    ],
 )
